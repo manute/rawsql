29ac6bc6ee6d82d778626122cee719a95dcf3ed0
Manuel Alonso <manuteali@gmail.com>
2015-11-08 11:54:18 +0100

    Spliting files into mods

---
+++ src/parser.rs
@@ -0,0 +1,41 @@
+use std::io::{Read, Result};
+use std::fs::File;
+use std::collections::HashMap;
+
+static TAG_NAME: &'static str = "-- name :";
+
+pub fn parse_file(path: &str) -> Result<HashMap<String, String>> {
+
+    let data_file = try!(read_file(path));
+    let mut result = HashMap::new();
+    let mut name = String::new();
+    let mut query = String::new();
+
+    for line in data_file.lines() {
+        if line.is_empty(){
+            continue;
+        }
+        if line.starts_with(TAG_NAME) {
+            name = line.replace(TAG_NAME,"").trim_left().to_string();
+            query = "".to_string();
+            continue;
+        }
+        if !name.is_empty() {
+            query = query + " " + &line.trim_left().to_string();
+        }
+        if !query.is_empty() && line.ends_with(";") {
+            result.insert(name, query);
+            name  = "".to_string();
+            query = "".to_string();
+        }
+    }
+
+    return Ok(result);
+}
+
+fn read_file(path: &str) -> Result<String> {
+    let mut file = try!(File::open(&path));
+    let mut data_file = String::new();
+    try!(file.read_to_string(&mut data_file));
+    return Ok(data_file);
+}
