20af27f6b455e225448bec1f6725b5eaab2f2b6d
malonso <manuel.alonso@corp.ovh.com>
2015-11-12 16:28:04 +0100

    Removing cascade on drop sql

--- examples/postgre.sql
+++ examples/postgre.sql
@@ -1,5 +1,5 @@
 -- name: drop-table-person
-DROP TABLE IF EXISTS "person" CASCADE;
+DROP TABLE IF EXISTS "person";
 
 -- name: create-table-person
 CREATE TABLE "person" (id SERIAL PRIMARY KEY, name  VARCHAR NOT NULL, data BYTEA);
